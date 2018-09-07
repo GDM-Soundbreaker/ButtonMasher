8316921df9c39cd7b64a3ababee61b4933073542
GDM-Soundbreaker <43067364+GDM-Soundbreaker@users.noreply.github.com>
2018-09-07 16:48:41 +0100

    Added message test

---
+++ Assets/Scripts/ButtonMasher.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonMasher : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("start method called");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log("update method called");
+	}
+}
